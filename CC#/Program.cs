a8938f7863d8a5061d1d62297499bf2059cd1fc3
smili <smili@DESKTOP-UOP8SF8>
2024-07-18 09:08:03 +0300

    Add project files.

---
+++ CC#/Program.cs
@@ -0,0 +1,103 @@
+﻿//Console.WriteLine("jusu Vardas");
+//string Vardas = Console.ReadLine();
+//Console.WriteLine("Pasisveikinimas");
+//Console.WriteLine("Labas , "+Vardas);
+
+//Console.WriteLine("Pirmas skaicius");
+//int A = int.Parse(Console.ReadLine());
+//Console.WriteLine("Antras skaicius");
+//int B = int.Parse(Console.ReadLine());
+//int Suma5 = A + B;
+//Console.WriteLine (Suma5);
+using System;
+using System.ComponentModel.Design;
+using System.ComponentModel.Design.Serialization;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Channels;
+
+//Console.WriteLine("gimimo metai");
+//int gimimoMetai = int.Parse(Console.ReadLine());
+//int sieMetai = int.Parse(Console.ReadLine());
+//int ateitis = int.Parse(Console.ReadLine());
+//int amzius = ( sieMetai - gimimoMetai + ateitis);
+//Console.WriteLine(amzius);
+
+//Console.WriteLine("virsutine dalys");
+//int virsutinedalys = int.Parse(Console.ReadLine());
+//Console.WriteLine("Desinioji dalys");
+//int Desiniojidalys = int.Parse(Console.ReadLine());
+//int plotas = ( virsutinedalys * Desiniojidalys);
+//Console.WriteLine(plotas);
+
+//Console.WriteLine("Prisistatymas");
+//String Prisistatymas = Console.ReadLine();
+//Console.WriteLine("Jusu Vardas");
+//string Vardas = Console.ReadLine();
+//Console.WriteLine("Jusu Pavarde");
+//String Pavarde = Console.ReadLine();
+//Console.WriteLine("kieakmetu");
+//string kiekmetu = Console.ReadLine();
+//Console.WriteLine("Skaicius");
+//int A = int.Parse(Console.ReadLine());
+//Console.WriteLine("metai");
+
+//Random random = new Random(); 
+
+//int num1 = random.Next(0, 4);
+// int num = random.Next(0, 4);
+//Console.WriteLine("num1 yra maziau uz num");
+//Console.WriteLine(num);
+//Console.WriteLine(num1);
+//int suma = num / num1;
+//Console.WriteLine(suma);
+
+
+//Random random = new Random();
+
+//int num1 = random.Next(0, 25);
+//int num2 = random.Next(0, 25);
+//int num3 = random.Next(0, 25);
+//Console.WriteLine(num1);
+//Console.WriteLine(num2);
+//Console.WriteLine(num3);
+
+//Random random = new Random();
+
+//int num = random.Next(0, 10);
+//int num1 = random.Next(0, 10);
+//int num2 = random.Next(0, 10);
+//Console.WriteLine(num);
+//    Console.WriteLine(num1);
+//Console.WriteLine(num2); Error...
+
+
+//Random random = new Random();
+
+//int num = random.Next(0,3);
+//int num1 = random.Next(0,3);
+//int num2 = random.Next(0,3);
+//int num3 = random.Next(0,3);
+//Console.WriteLine(num);
+//Console.WriteLine(num1);
+//Console.WriteLine(num2);
+//Console.WriteLine(num3);
+// atsakimas (vienas 1 vieanas 2 du 0 );
+
+//Task 6 -
+
+//Console.WriteLine("Pirmas skaicius");
+//int A = int.Parse(Console.ReadLine());
+//Console.WriteLine("Antras skaicius");
+//int B = int.Parse(Console.ReadLine());
+//int Suma = A - 30;
+//int Suma1 = B - 80;
+//Console.WriteLine (Suma);
+//Console.WriteLine(Suma1);
+
+//Random random = new Random();
+//int num = random.Next(5, 3000);
+//Console.WriteLine(num);
+//Console.WriteLine("Pirmas skaicius");
+//int A = int.Parse(Console.ReadLine());
+//Console.WriteLine("Antras skraicius");
+//int B = int.Parse(Console.ReadLine()); ERROR....
